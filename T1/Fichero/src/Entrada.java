2db89ec3b08c19335c47ea52fb3875a0d822a77f
Develop and System <developandsystem@gmail.com>
2024-09-20 18:05:28 +0200

    update

--- T1/Fichero/src/Entrada.java
+++ T1/Fichero/src/Entrada.java
@@ -1,6 +1,7 @@
 import controller.GestorFicheros;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Entrada {
@@ -10,7 +11,10 @@ public class Entrada {
         GestorFicheros gestorFicheros = new GestorFicheros();
         // gestorFicheros.lecturaDirectorios("src/resources/directorio");
         // gestorFicheros.lecturaDirectorios("src/resources/ficheros");
-        gestorFicheros.lecturaRecursiva("/users/borja/documents/github");
+        // gestorFicheros.lecturaRecursiva("/users/borja/documents/github");
+        gestorFicheros.lecturaTextoPlano("src/resources/ficheros/lectura.txt");
+
+
 
         // instancias de objetos y llamadas a metodos
         // OBJETO CLASE 1

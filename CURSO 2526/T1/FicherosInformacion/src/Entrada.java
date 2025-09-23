b3818369636690f8650ad997f467ca58802469a6
Develop and System <developandsystem@gmail.com>
2025-09-23 20:42:41 +0200

    update

--- CURSO 2526/T1/FicherosInformacion/src/Entrada.java
+++ CURSO 2526/T1/FicherosInformacion/src/Entrada.java
@@ -2,5 +2,6 @@ public class Entrada {
 
     public static void main(String[] args) {
         System.out.println("Este ejemplo es para el uso de IDEA Proyect");
+        System.out.println("Este mensaje es para ver como se usa GitDesktop");
     }
 }

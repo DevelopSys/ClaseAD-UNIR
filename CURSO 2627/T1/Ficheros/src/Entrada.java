35ab32d5b68b15df24e517653d97d6487fab950d
Develop and System <developandsystem@gmail.com>
2026-09-25 17:55:34 +0200

    update

--- CURSO 2627/T1/Ficheros/src/Entrada.java
+++ CURSO 2627/T1/Ficheros/src/Entrada.java
@@ -1,14 +1,43 @@
 import controller.FileContoller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Entrada {
 
     // main(String[]args){}
     // main(){}
     public static void main(String[] args) {
         FileContoller fileContoller = new FileContoller();
+
+        // lecturas por consola - fichero
+        Scanner lector = new Scanner(System.in);
+        // lecturas por consola -> solo se pueden leer string
+        // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
         System.out.println("Proyecto de gestion de ficheros");
-        fileContoller.createFile("src/resources/");
-        System.out.println("Terminando");
+        /*
+        Creacion y tratamiento de error en fichero
+        do {
+            System.out.println("Introduce el nombre del fichero");
+            String nombreFichero = lector.nextLine();
+            try {
+                fileContoller.crearFichero(nombreFichero);
+                break;
+            } catch (IOException e) {
+                System.out.println("Error a la hora de crear el fichero");
+            }
+        }while (true);
+        System.out.println("Fichero creado correctamente");
+        // terminamos el uso del flujo -> cerrando el flujo*/
+        String nombre = lector.nextLine();
+        fileContoller.crearCarpeta(nombre);
+        lector.close();
+
     }
 
 }
--- CURSO 2627/T1/Ficheros/src/controller/FileContoller.java
+++ CURSO 2627/T1/Ficheros/src/controller/FileContoller.java
@@ -1,21 +1,29 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileContoller {
 
-    public void createFile(String path){
-        // path -> src/resources
-        // fichero logico -> solo existe en memoria
-        File file = new File(path+"file.txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            System.out.println("Error en la creacion");
-        }
-        System.out.println(file.isDirectory());
+    private String basePath = "src/resources/";
 
+    public void crearFichero(String path) throws IOException {
+        // src/resources/example.txt
+        // directorio (carpeta) fichero_final (alemento con extension)
+        File file = new File(basePath + path);
+        if(file.exists()){
+            System.out.println("El fichero ya existe");
+        } else {
+            file.createNewFile();
+        }
     }
 
+    public void crearCarpeta(String path) {
+        // src/resources/example.txt
+        // directorio (carpeta) fichero_final (alemento con extension)
+        File file = new File(basePath + path);
+        file.mkdirs();
+    }
 }
